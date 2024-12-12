69dea38227decbbf3e7cef5dcdbc1b9bfaf1c6a4
abed-naanaa <abednaanaa12@gmail.com>
2024-12-12 17:23:04 +0000

    f

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,14 +1,13 @@
 #include <unistd.h>
+
 /**
-*
 * main - Entry point of the program
 *
 * Description: Prints "_putchar" followed by a new line to stdout
 *
-* Return: Always 0(sucess)
+* Return: Always 0 (success)
 **/
 int main(void)
-	
 {
 char str[] = "_putchar\n";
 write(1, str, sizeof(str) - 1);
