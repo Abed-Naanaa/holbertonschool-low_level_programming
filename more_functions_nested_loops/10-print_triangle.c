a4440c812465fdea82c59b05dce686c4fde11a1f
abed-naanaa <abednaanaa12@gmail.com>
2024-12-13 15:28:34 +0000

    betty all

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -1,24 +1,22 @@
 #include "main.h"
-
 /**
- * print_triangle - Prints a right-angled triangle using '#' characters.
- * @size: The size of the triangle.
- */
+* print_triangle - Prints a right-angled triangle using '#' characters.
+* @size: The size of the triangle.
+*/
 void print_triangle(int size)
 {
-    int i, j;
-
-    if (size <= 0)
-        _putchar('\n');
-    else
-    {
-        for (i = 1; i <= size; i++)
-        {
-            for (j = 1; j <= size - i; j++)
-                _putchar(' ');  /* Print spaces before the '#' */
-            for (j = 1; j <= i; j++)
-                _putchar('#');  /* Print the '#' characters */
-            _putchar('\n');    /* New line after each row */
-        }
-    }
+int i, j;
+if (size <= 0)
+_putchar('\n');
+else
+{
+for (i = 1; i <= size; i++)
+{
+for (j = 1; j <= size - i; j++)
+_putchar(' ');  /* Print spaces before the '#' */
+for (j = 1; j <= i; j++)
+_putchar('#');  /* Print the '#' characters */
+_putchar('\n');    /* New line after each row */
+}
+}
 }
