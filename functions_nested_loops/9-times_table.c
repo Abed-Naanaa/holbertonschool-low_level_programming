84cc7f787e46efcc865fafdd4f87d09a6649a803
abed-naanaa <abednaanaa12@gmail.com>
2024-12-13 05:41:14 +0000

    9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -9,25 +9,25 @@ void times_table(void)
 {
     int i, j, result;
 
-    for (i = 0; i < 10; i++)  // Outer loop for rows (0 to 9)
+    for (i = 0; i < 10; i++)
     {
-        for (j = 0; j < 10; j++)  // Inner loop for columns (0 to 9)
+        for (j = 0; j < 10; j++)
         {
             result = i * j;
-            
+
             if (j != 0)
-                _putchar(',');  // Add comma before printing results from the second column
-            if (result < 10)  // If result is a single digit
+                _putchar(',');
+            if (result < 10)
             {
-                _putchar(' ');  // Add leading space for single-digit numbers
+                _putchar(' ');
                 _putchar(result + '0');
             }
-            else  // If result is a double digit
+            else
             {
-                _putchar(result / 10 + '0');  // Print tens digit
-                _putchar(result % 10 + '0');  // Print ones digit
+                _putchar(result / 10 + '0');
+                _putchar(result % 10 + '0');
             }
         }
-        _putchar('\n');  // New line after each row
+        _putchar('\n');
     }
 }
