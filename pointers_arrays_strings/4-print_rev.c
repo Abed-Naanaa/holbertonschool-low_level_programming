ce55fdd52016363f5f6a60212a12cdc8463d2b1d
abed-naanaa <abednaanaa12@gmail.com>
2024-12-17 19:01:32 +0000

    q4

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,18 +1,19 @@
 #include "main.h"
+
 /**
- * _puts - Prints a string in reverse, followed by a new line
- * @str: Pointer to the string to be printed
+ * print_rev - Prints a string in reverse, followed by a new line
+ * @s: Pointer to the string to be printed
  */
-void _puts(char *str)
+void print_rev(char *s)
 {
 int i;
 
-for (i = 0; str[i] != '\0'; i++)
+for (i = 0; s[i] != '\0'; i++)
 {
 }
 for (i--; i >= 0; i--)
 {
-_putchar(str[i]);
+_putchar(s[i]);
 }
 _putchar('\n');
 }
