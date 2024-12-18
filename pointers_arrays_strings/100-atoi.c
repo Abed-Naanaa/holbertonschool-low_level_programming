83590e1fc62a0dec4c332eae524b3c4a6c36f165
abed-naanaa12 <abednaanaa12@gmail.com>
2024-12-18 18:14:40 +0000

    100

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <limits.h>
 
 /**
 *_atoi - Converts a string to an integer.
@@ -7,10 +8,8 @@
 */
 int _atoi(char *s)
 {
-int i = 0;
-int sign = 1;
-int result = 0;
-int started = 0;
+int i = 0, sign = 1;
+unsigned int result = 0; /* Use unsigned to handle overflow detection */
 
 while (s[i] != '\0')
 {
@@ -18,12 +17,17 @@ if (s[i] == '-')
 sign *= -1;
 else if (s[i] >= '0' && s[i] <= '9')
 {
-started = 1;
+/* Check for potential overflow before adding the next digit */
+if (result > (unsigned int)(INT_MAX / 10) ||
+   (result == (unsigned int)(INT_MAX / 10) && (s[i] - '0') > (INT_MAX % 10)))
+{
+return (sign == 1 ? INT_MAX : INT_MIN);
+}
 result = result * 10 + (s[i] - '0');
 }
-else if (started)
+else if (result > 0)
 break;
 i++;
 }
-return (result * sign);
+return ((int)(result * sign));
 }
