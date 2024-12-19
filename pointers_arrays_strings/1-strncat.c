d9a385c5e6d19896d8e8d369bcc59135bf35e4d0
abed-naanaa12 <abednaanaa12@gmail.com>
2024-12-19 13:01:59 +0000

    pt2-q2

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,26 +1,31 @@
 #include "main.h"
 
 /**
-*_strcat - Concatenates two strings.
+*_strncat - Concatenates two strings using at most n bytes from src.
 *@dest: The destination string.
 *@src: The source string.
+*@n: The maximum number of bytes to use from src.
 *
 *Return: A pointer to the resulting string `dest`.
 */
 char *_strncat(char *dest, char *src, int n)
 {
-int i;
 char *dest_end = dest;
+int i = 0;
+/* Find the end of the dest string */
 while (*dest_end != '\0')
 {
 dest_end++;
 }
-for (i=0; i<n; i++)
+/* Append src to dest, at most n bytes */
+while (i < n && *src != '\0')
 {
 *dest_end = *src;
 dest_end++;
 src++;
+i++;
 }
+/* Add the null terminator */
 *dest_end = '\0';
 return (dest);
 }
