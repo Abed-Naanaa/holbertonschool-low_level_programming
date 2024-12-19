c950a1c74a431e799dce9d5cfabe4c0266d72bbe
abed-naanaa12 <abednaanaa12@gmail.com>
2024-12-19 15:14:41 +0000

    pt2-q6

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+* cap_string - Capitalizes all words in a string.
+* @str: The string to be modified.
+*
+* Return: A pointer to the modified string.
+*/
+char *cap_string(char *str)
+{
+int i = 0;
+int j;
+char separators[] = " \t\n,;.!?\"(){}";
+
+while (str[i] != '\0')
+{
+if (i == 0 || strchr(separators, str[i - 1]) != NULL)
+{
+if (str[i] >= 'a' && str[i] <= 'z')
+{
+str[i] = str[i] - 'a' + 'A';
+}
+}
+i++;
+}
+return (str);
+}
