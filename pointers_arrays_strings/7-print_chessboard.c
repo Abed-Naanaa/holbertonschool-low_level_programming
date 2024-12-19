3a057869ff75d2099472fb909bf3096674fd8a99
abed-naanaa12 <abednaanaa12@gmail.com>
2024-12-19 21:48:59 +0000

    pt3-q6

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_chessboard - prints the chessboard
+ * @a: a 2D array representing the chessboard
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++) /* Loop through each row */
+	{
+		for (j = 0; j < 8; j++) /* Loop through each column */
+		{
+			putchar(a[i][j]); /* Print each character */
+		}
+		putchar('\n'); /* Print a new line after each row */
+	}
+}
