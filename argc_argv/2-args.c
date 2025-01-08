1cc06b24e3ffacf4904736f3c3c2e35cf77f94ff
abed-naanaa <abednaanaa12@gmail.com>
2025-01-08 17:16:48 +0000

    q2

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+* main - Prints all arguments passed to the program.
+* @argc: The number of command-line arguments.
+* @argv: An array of strings containing the program name and arguments.
+*
+* Return: Always 0 (success).
+*/
+
 int main(int argc, char *argv[])
 {
 	int i = 0;
