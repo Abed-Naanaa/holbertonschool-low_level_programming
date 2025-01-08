a79f67480063f31c3a8fdfb7ac6e8aa53e75a847
abed-naanaa <abednaanaa12@gmail.com>
2025-01-08 16:54:06 +0000

    d

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the number of arguments.
+ * @argc: The number of command-line arguments.
+ * @argv: An array of strings containing the program name and arguments.
+ *
+ * Return: Always 0 (success).
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d/n", argc);
+	return (0);
+}
