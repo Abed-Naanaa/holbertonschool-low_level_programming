9e8c6b711d5ca3b44da5a9c765fa64df2ebf1676
abed-naanaa <abednaanaa12@gmail.com>
2025-01-15 18:14:29 +0000

    1,2 and 3

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -14,4 +14,11 @@ struct dog
 	char *owner;
 };
 
+/* Define a new type dog_t as a new name for struct dog */
+typedef struct dog dog_t;
+
+/* Function prototypes */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
 #endif /* DOG_H */
