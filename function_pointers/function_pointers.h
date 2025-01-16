ee0ae2b7d830a842a86633846323ea6cf90f515c
abed-naanaa <abednaanaa12@gmail.com>
2025-01-16 17:29:59 +0000

    q1

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_HEADER_FILE
 #define MAIN_HEADER_FILE
+#include <stddef.h>
 int _putchar(int c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
