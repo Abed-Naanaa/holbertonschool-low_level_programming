7ac4aabeff646b1fcdaffc953a5d743de117680e
abed-naanaa <abednaanaa12@gmail.com>
2025-01-17 13:47:35 +0000

    betty

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -7,37 +7,37 @@
  * @argc: The number of arguments.
  * @argv: The array of arguments.
  *
- * Return: 0 on success, or exits with an error code on failure.
+ * Return: 0 on success, or exits with an error code otherwise.
  */
 int main(int argc, char *argv[])
 {
-    int num1, num2, result;
-    int (*operation)(int, int);
+	int num1, num2, result;
+	int (*op_func)(int, int);
 
-    if (argc != 4)
-    {
-        printf("Error\n");
-        exit(98);
-    }
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-    num1 = atoi(argv[1]);
-    num2 = atoi(argv[3]);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
 
-    operation = get_op_func(argv[2]);
-    if (operation == NULL)
-    {
-        printf("Error\n");
-        exit(99);
-    }
+	op_func = get_op_func(argv[2]);
+	if (!op_func)
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
-    if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
-    {
-        printf("Error\n");
-        exit(100);
-    }
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 
-    result = operation(num1, num2);
-    printf("%d\n", result);
+	result = op_func(num1, num2);
+	printf("%d\n", result);
 
-    return (0);
+	return (0);
 }
