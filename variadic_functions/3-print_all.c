dafb2c324a2cdeb486594b7b25d0c53be86bfc73
abed-naanaa <abednaanaa12@gmail.com>
2025-01-17 14:16:40 +0000

    3

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -15,25 +15,25 @@ va_start(args, format);
 
 while (format && format[i])
 {
-if (format[i] == 'c' || format[i] == 'i' ||
-format[i] == 'f' || format[i] == 's')
+switch (format[i])
 {
-printf("%s", sep);
-if (format[i] == 'c')
-printf("%c", va_arg(args, int));
-else if (format[i] == 'i')
-printf("%d", va_arg(args, int));
-else if (format[i] == 'f')
-printf("%f", va_arg(args, double));
-else if (format[i] == 's')
-{
+case 'c':
+printf("%s%c", sep, va_arg(args, int));
+break;
+case 'i':
+printf("%s%d", sep, va_arg(args, int));
+break;
+case 'f':
+printf("%s%f", sep, va_arg(args, double));
+break;
+case 's':
 str = va_arg(args, char *);
 if (str == NULL)
 str = "(nil)";
-printf("%s", str);
+printf("%s%s", sep, str);
+break;
 }
 sep = ", ";
-}
 i++;
 }
 
