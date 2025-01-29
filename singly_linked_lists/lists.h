3278619a8d309bc4a3370c6134a2209ec6682d3d
abed-naanaa12 <abednaanaa12@gmail.com>
2025-01-29 21:50:22 +0000

    0

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,5 +1,13 @@
 #ifndef MAIN_HEADER_FILE
 #define MAIN_HEADER_FILE
+#include <stddef.h>
+
+typedef struct list_s {
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
+
 int _putchar(int c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
