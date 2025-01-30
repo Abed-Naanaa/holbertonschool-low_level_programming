16f1a9fd3ebad0d32c21d7d96801d5fad776a960
abed-naanaa <abednaanaa12@gmail.com>
2025-01-30 13:52:21 +0000

    3

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -14,6 +14,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *new_node;
 	list_t *last_node = *head;
 	char *dup_str;
+	int len = 0;
 
 	new_node = malloc(sizeof(list_t));
 	if (!new_node)
